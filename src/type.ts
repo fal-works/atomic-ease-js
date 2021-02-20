d5574c9cdc8ad5342b5e0beb383da44d34849526
FAL <falworks.contact@gmail.com>
2021-02-20 17:08:37 +0900

    docs: add comment

--- src/type.ts
+++ src/type.ts
@@ -1,5 +1,6 @@
 /**
- * @param x Any ratio.
+ * Easing function.
+ * @param x Any input ratio. Typically between `0` and `1`.
  * @returns Eased ratio. Typically `0` if x=0, `1` if x=1.
  */
 export type Easing = (x: number) => number;
